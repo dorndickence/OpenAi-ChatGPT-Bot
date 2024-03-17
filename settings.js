fde04ebd2c46f1a24c549f683a4d79b48113f61a
Dornavan <130479724+dorndickence@users.noreply.github.com>
2024-03-17 03:17:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-var SESSION_ID = '254759245741'
+var SESSION_ID = ''
 global.AI_PASSWORD = 'obturator-foramen'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
